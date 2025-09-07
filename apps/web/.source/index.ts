0a2c79e6a4ebe4813269c840f1ded22d30170734
Noah Hill <noahhill576@aol.com>
2025-09-08 00:46:31 +0700

    first commit

---
+++ apps/web/.source/index.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck -- skip type checking
+import * as docs_1 from "../content/docs/test.mdx?collection=docs&hash=1757266620578"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1757266620578"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"E:/NextJS/enterteinment/nha24hcafe/apps/web/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"test.mdx","absolutePath":"E:/NextJS/enterteinment/nha24hcafe/apps/web/content/docs/test.mdx"}, data: docs_1 }], [])
